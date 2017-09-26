08886fc2d3be3605cd967cf46fe1ce16305c6379
Nigel Skeels <nigels@tlk.lan>
2017-09-26 16:01:35 +0100

    smart

---
+++ smartted/jackdaw.speechrecog.js
@@ -0,0 +1,144 @@
+﻿Jackdaw={}
+Jackdaw.Speechrecog = ( function() {
+
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = "en-GB";
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    
+    // var output;
+    var status;
+    var curruserid;
+    var currenttranslation = [];
+    var transcriptionid;
+    var speechend=false;
+    var currentlystarted=false;
+
+    function Init(){
+        console.log("Hello Speech recog");
+        recognition = new webkitSpeechRecognition();
+        recognition.lang = "en-GB";
+        recognition.continuous = true;
+        recognition.interimResults = true;
+        recognition.maxAlternatives = 1;
+        
+        // var output;
+        currenttranslation = [];
+        speechend=false;
+        currentlystarted=false;
+    } 
+
+    function Startrecog(userid,_transcriptionid){
+        // if(Jackdaw.Options.speechrecognitionenabled()==true){
+            if(currentlystarted==false){
+                currentlystarted=true;
+                curruserid=userid;
+                transcriptionid=_transcriptionid;
+                try {
+                    recognition.start()
+                }catch(err){
+                    console.info("Start speech transcription",err)
+                }
+            }
+        // }
+    }
+
+    function Stoprecog(userid,_transcriptionid){
+        // if(Jackdaw.Options.speechrecognitionenabled()==true){
+            currentlystarted=false;
+            curruserid=userid;
+            transcriptionid=_transcriptionid;
+            recognition.stop();
+        // }
+    }
+
+    recognition.onresult = function(event) { 
+      var test = event;
+      console.log("Speech recog event",test)
+      // output.innerHTML="";
+
+      currenttranslation=[];
+
+      for (var i = 0; i < event.results.length; i++) {
+         
+          // output.innerHTML=event.results[i][0].transcript;
+        var nativerecorderevent = new CustomEvent('speechrecogevent', { 'detail': event.results[i][0].transcript});
+        document.body.dispatchEvent(nativerecorderevent);
+
+         
+          if(event.results[i].isFinal==true){
+            
+            currenttranslation.push(event.results[i][0].transcript);
+            
+         
+          }
+      };
+
+    }
+
+
+    // recognition.onaudiostart = function(event) { 
+    //     console.log("Speech recog ","audiostart");
+    // }
+
+    // recognition.onsoundstart = function(event) { 
+    //     console.log("Speech recog ","soundstart");
+    // }
+
+    // recognition.onspeechstart = function(event) { 
+    //     console.log("Speech recog ","speechstart");
+    // }
+
+    recognition.onspeechend  = function(event) { 
+        console.log("Speech recog ","speechend");
+
+        speechend=true;
+
+        
+    }
+
+    // recognition.onsoundend  = function(event) { 
+    //    console.log("Speech recog ","soundend");
+    // }
+
+    // recognition.onaudioend = function(event) { 
+    //    console.log("Speech recog ","audioend");
+    // }
+
+    // recognition.onnomatch = function(event) { 
+    //     console.log("Speech recog ","nomatch");
+    // }
+
+    // recognition.onstart = function(event) { 
+    //     console.log("Speech recog ","start");
+    // }
+
+    recognition.onend = function(event) { 
+        console.log("Speech recog ","end");
+
+        var joined;
+        if(currenttranslation.length==1){
+            joined = currenttranslation[0];
+        }else{
+            joined = currenttranslation.join();
+        }
+
+        var nativerecorderevent = new CustomEvent('speechrecogevent', { 'detail': joined});
+        document.body.dispatchEvent(nativerecorderevent);
+
+    }
+
+
+    recognition.onerror = function(event) {
+           console.warn("Speech to text error",event)
+    }
+
+
+    return {
+                  init:Init,
+            startrecog:Startrecog,
+             stoprecog:Stoprecog
+    };
+  
+} ( Jackdaw.Speechrecog || {} ));
+    
---
+++ smartted/jackdaw.speechsynth.js
@@ -0,0 +1,101 @@
+﻿Jackdaw.Speechsynth = ( function() {
+
+
+    var synth = window.speechSynthesis;
+    var inputForm
+    var inputTxt
+    var voiceSelect
+
+    var pitch
+    var pitchValue
+    var rate
+    var rateValue
+
+    var voices = [];
+
+    function Init(){
+        console.log("Hello Speech synth");
+       
+        inputForm = document.querySelector('form');
+        inputTxt = document.querySelector('.txt');
+        voiceSelect = document.querySelector('select');
+
+        pitch = document.querySelector('#pitch');
+        pitchValue = document.querySelector('.pitch-value');
+        rate = document.querySelector('#rate');
+        rateValue = document.querySelector('.rate-value');
+
+        voices = [];
+
+        // inputForm.onsubmit = function(event) {
+        //   event.preventDefault();
+
+        //   speak();
+
+        //   inputTxt.blur();
+        // }
+
+        pitch.onchange = function() {
+          pitchValue.textContent = pitch.value;
+        }
+
+        rate.onchange = function() {
+          rateValue.textContent = rate.value;
+        }
+
+        voiceSelect.onchange = function(){
+          speak("Changed voice to this one");
+        }
+        populateVoiceList();
+    } 
+
+   function populateVoiceList() {
+      voices = synth.getVoices();
+      var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+      voiceSelect.innerHTML = '';
+      for(i = 0; i < voices.length ; i++) {
+        var option = document.createElement('option');
+        option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+        
+        if(voices[i].default) {
+          option.textContent += ' -- DEFAULT';
+        }
+
+        option.setAttribute('data-lang', voices[i].lang);
+        option.setAttribute('data-name', voices[i].name);
+        voiceSelect.appendChild(option);
+      }
+      voiceSelect.selectedIndex = 3;
+    }
+
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+      speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
+
+    function speak(what){
+
+        var utterThis = new SpeechSynthesisUtterance(what);
+        var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+        for(i = 0; i < voices.length ; i++) {
+          if(voices[i].name === selectedOption) {
+            utterThis.voice = voices[i];
+          }
+        }
+        utterThis.pitch = pitch.value;
+        utterThis.rate = rate.value;
+        synth.speak(utterThis);
+      
+    }
+
+   
+
+    
+
+
+    return {
+                  init:Init,
+                  speak:speak
+    };
+  
+} ( Jackdaw.Speechsynth || {} ));
+    
